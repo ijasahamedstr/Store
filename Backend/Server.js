98cdec6def5c06a84f6cf32318b6cb4e8f5ffc6b
ijasahamedstr <ijasahamed.str@gmail.com>
2024-11-04 16:09:40 +0200

    commit

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,11 +1,30 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from 'express';
+import cors from 'cors';
+import connectDB from './lib/db.js';
 
+const app = express();
+const port = 8000;
+
+// Connect to the database
+connectDB();
+
+// Middleware
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: 'GET,POST,PUT,DELETE',
+  credentials: true
+}));
+app.use(express.json()); // Middleware to parse JSON bodies
+
+// Define a route handler for the default home page
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
+//CLIENT -> MIDDLEWARE -> SERVER
+
+
+// Start the server
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
